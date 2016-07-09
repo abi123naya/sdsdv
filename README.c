77718f6b2e4549079735532abd4a1f9b7ea95796
abi123naya <connect2abinaya@gmail.com>
2016-07-09 15:45:08 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,42 @@
+# sdsdv
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],b[10][10],k=0,m,i,j,n,temp=0;
+clrscr();
+scanf("%d %d",&m,&n);
+for(i=0;i<n*m;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<m*n;i++)
+{
+for(j=i+1;j<n*m;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+b[i][j]=a[k];
+k++;
+}
+}
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+printf(" %d",b[i][j]);
+}
+printf("\n");
+}
+getch();
+}
